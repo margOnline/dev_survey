8b09f842236711f45df7f1820ab1a8455d3d852c
Margo Urey <margo@margonline.co.uk>
2015-08-09 23:49:08 +0100

    views for admin_questions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,3 +1,10 @@
 module QuestionsHelper
+  def question_groups_dropdown(form)
+    form.select(
+      :question_group_id,
+      QuestionGroup.all.map { |qg| [qg.name, qg.id] },
+      { prompt: 'Select one' }
+    )
+  end
 
 end
